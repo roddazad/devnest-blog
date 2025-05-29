e68ec00ab2dadb94f5012fbc09dba6ba66fce94c
roddazad <roddazad@gmail.com>
2025-05-29 10:46:01 -0700

    Chnaged the a tag to link

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
+import Link from 'next/link';
 export default function HomePage() {
   return (
     <section className="text-center py-16">
       <h1 className="text-4xl md:text-5xl font-bold mb-4 text-gray-900">Welcome to DevNest 🪺</h1>
       <p className="text-gray-600 text-lg mb-8">Where developer stories hatch and grow.</p>
-      <a
+      <Link
         href="/blog"
         className="inline-block bg-teal-500 hover:bg-teal-600 text-white font-medium py-3 px-6 rounded transition"
       >
         Read the Blog
-      </a>
+      </Link>
     </section>
   );
 }
